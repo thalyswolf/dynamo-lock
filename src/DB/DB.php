1e9743db53b76d006cb54028bc1691cff1ca8546
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-20 15:21:46 -0300

    db

--- src/DB/DB.php
+++ src/DB/DB.php
@@ -4,6 +4,7 @@ namespace App\DB;
 
 interface DB {
     public function connect();
+    public function create(array $statement);
     public function update(array $statement);
     public function findOne(array $statement);
     public function beginTransaction();
