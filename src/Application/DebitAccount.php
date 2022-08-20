92792d192dc480e563c4302d81c5ab4787abf984
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-20 13:19:48 -0300

    implemented debit account application

--- src/Application/DebitAccount.php
+++ src/Application/DebitAccount.php
@@ -2,6 +2,21 @@
 
 namespace App\Application;
 
-class DebitAccount {
+use App\Entity\Transaction;
+use App\Repository\AccountContract;
+use stdClass;
+
+class DebitAccount implements ApplicationContract {
     
+    public function __construct(private AccountContract $accountRepository)
+    {}
+
+    public function execute(stdClass $inputBoundary): stdClass
+    {
+        $account = $this->accountRepository->getById($inputBoundary->accountId);
+        $transaction = new Transaction($inputBoundary->amount, $account);
+        $transaction = $this->accountRepository->debit($transaction);
+        return new stdClass;
+    }
+
 }
