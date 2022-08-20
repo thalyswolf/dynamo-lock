6991942c9243b45dede512c993a619da1356314d
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-20 15:22:06 -0300

    update repository

--- src/Repository/AccountRepositoryDynamoWithOptimisticLock.php
+++ src/Repository/AccountRepositoryDynamoWithOptimisticLock.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 use App\DB\DB;
 use App\Entity\Account;
 use App\Entity\Transaction;
+use Exception;
+use Aws\DynamoDb\Exception\DynamoDbException;
 
 class AccountRepositoryDynamoWithOptimisticLock implements AccountContract {
     
@@ -24,13 +26,60 @@ class AccountRepositoryDynamoWithOptimisticLock implements AccountContract {
 
         $result = $this->db->findOne($statement);
 
-        var_dump($result);
-
-        return new Account('asd', 100);
+        return new Account($result['Item']['id']['S'], (int) $result['Item']['balance']['N']);
     }
     public function debit(Transaction $transaction): Transaction
     {
-        return new Transaction(100, new Account('asd', 100));
+        try {
+            $statementGetAccount = [
+                'ConsistentRead' => true,
+                'TableName' => 'Accounts',
+                'Key' =>[
+                    'id'   => ['S' => $transaction->getAccountId()]
+                ]
+            ];
+            $resultAccount = $this->db->findOne($statementGetAccount);
+            $account = new Account($resultAccount['Item']['id']['S'], (int) $resultAccount['Item']['balance']['N']);
+            if ($account->getBalance() < $transaction->getAmount()) throw new Exception("Insuficient funds");
+    
+            $debitStatement = [
+                'TableName' => 'Accounts',
+                'ConditionExpression' => 'version = :version',
+                'Key' => [
+                    'id' => ['S' => $transaction->getAccountId()]
+                ],
+                'ExpressionAttributeValues' => [
+                    ':amount' => ['N' => (string)$transaction->getAmount()],
+                    ':version' => ['N' => (string) $resultAccount['Item']['version']['N']],
+                    ':count' =>['N' => '1']
+                ],
+                'UpdateExpression' => 'SET balance = balance - :amount, version = version + :count',
+                'ReturnValues' => 'ALL_NEW'
+            ];
+    
+            $result = $this->db->update($debitStatement);
+
+            $newbalance = $result['Attributes']['balance']['N'];
+
+            // Generate random ID
+            $id = random_int(1, 1000000000);
+            $statementTransaction = [
+                'TableName' => 'Transactions',
+                'Item' => [
+                    'id' => array('N' => "$id"),
+                    'amount' => array('N' => (string) $transaction->getAmount()),
+                    'balanceAter' => array('N' => $newbalance),
+                    'accountId' => array('S' => $transaction->getAccountId())
+                ]
+            ];
+
+            $this->db->create($statementTransaction);
+
+        } catch (DynamoDbException $lock) {
+            if ($lock->getAwsErrorCode() == 'ConditionalCheckFailedException') return $this->debit($transaction);
+        }
+
+        return $transaction;
     }
 
 }
