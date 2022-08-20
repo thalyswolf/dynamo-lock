e2325e299f69de1050a26ce05bdd99388b8b8263
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-20 13:24:55 -0300

    setup dynamodb

---
+++ src/DB/DynamoDB.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DB;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\Credentials\CredentialProvider;
+
+class DynamoDB implements DB {
+
+    public function connect()
+    {
+
+    }
+
+    public function execute()
+    {
+
+    }
+    
+    public function findOne()
+    {
+
+    }
+
+    public function beginTransaction()
+    {
+
+    }
+
+    public function endTransaction()
+    {
+
+    }
+}
