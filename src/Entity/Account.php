a491e74c7566ad74a9e824cc4302afa8503a79f3
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-20 13:00:30 -0300

    created account

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -3,5 +3,21 @@
 namespace App\Entity;
 
 final class Account {
+    
+    public function __construct(
+        private string $id,
+        private int $balance
+    )
+    {}
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
 
 }
