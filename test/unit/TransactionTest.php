d9cc04eaa331c81b0cc8d1e967b34cf13dc82e58
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-21 11:52:34 -0300

    Setup transaction test

---
+++ test/unit/TransactionTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests;
+
+use App\Entity\Account;
+use App\Entity\Transaction;
+use DomainException;
+use PHPUnit\Framework\TestCase;
+
+class TransactionTest extends TestCase {
+    
+}
