09f399b71f7e599d871036359fa31c92dc1bd9b1
Thalys Wolf <thalyswolf01@gmail.com>
2022-08-21 11:52:09 -0300

    To ensure creating account object

--- test/unit/AccountTest.php
+++ test/unit/AccountTest.php
@@ -6,4 +6,12 @@ use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase {
     
+    public function testShallCreateAccountObject()
+    {
+        $account = new Account('id', 100);
+
+        $this->assertSame(100, $account->getBalance());
+        $this->assertSame('id', $account->getId());
+    }
+
 }
